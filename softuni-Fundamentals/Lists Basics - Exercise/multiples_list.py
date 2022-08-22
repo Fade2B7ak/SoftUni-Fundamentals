eeee90edf67ffb0d4f3408008dab296f02196579
Daniel Yordanov <103177811+Fade2B7ak@users.noreply.github.com>
2022-08-22 16:21:51 +0300

    Update multiples_list.py

--- softuni-Fundamentals/Lists Basics - Exercise/multiples_list.py
+++ softuni-Fundamentals/Lists Basics - Exercise/multiples_list.py
@@ -6,3 +6,14 @@ list_of_numbers = []
 for multiplayer in range(1, count + 1):
     list_of_numbers.append(factor_number * multiplayer)
 print(list_of_numbers)
+
+#Functional Solving
+def multiple(factor, count):
+    list_of_nums = []
+
+    for num in range(1, count + 1):
+        list_of_nums.append(factor * num)
+    print(list_of_nums)
+
+
+multiple(factor=int(input()), count=int(input()))
