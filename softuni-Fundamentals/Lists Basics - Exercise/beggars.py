c11d634f3e57023e84b72537075ca2f9903860c6
Daniel Yordanov <103177811+Fade2B7ak@users.noreply.github.com>
2022-08-22 16:20:48 +0300

    Update beggars.py

--- softuni-Fundamentals/Lists Basics - Exercise/beggars.py
+++ softuni-Fundamentals/Lists Basics - Exercise/beggars.py
@@ -16,3 +16,21 @@ while beggars_turn < count_of_beggards:
     beggars_turn += 1
 
 print(collected_money)
+
+#Functional Solving
+def beggars(money, count_of_beggars):
+    money_earned = []
+
+    for gold in money:
+        money_earned.append(int(gold))
+
+    money_took = [0] * count_of_beggars
+
+    for index in range(len(money_earned)):
+        beggar = index % count_of_beggars
+        money_took[beggar] += money_earned[index]
+
+    print(money_took)
+
+
+beggars(money=input().split(","), count_of_beggars=int(input()))
