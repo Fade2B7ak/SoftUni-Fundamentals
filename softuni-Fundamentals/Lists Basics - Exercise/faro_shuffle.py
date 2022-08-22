be87f2d62ee93c8c603aa2e87ea42ada289054a0
Daniel Yordanov <103177811+Fade2B7ak@users.noreply.github.com>
2022-08-22 16:20:07 +0300

    Update faro_shuffle.py

--- softuni-Fundamentals/Lists Basics - Exercise/faro_shuffle.py
+++ softuni-Fundamentals/Lists Basics - Exercise/faro_shuffle.py
@@ -13,3 +13,25 @@ for shuffle in range(number_of_shuffles):
     card_deck = final_deck
 
 print(card_deck)
+
+#Functional solving of the problem.
+def farro_shuffle(card_deck, count_of_shuffles):
+    final_deck = []
+
+    # Looping through the shuffles, so we can split the deck on 2 hals.
+    for shuffle in range(count_of_shuffles):
+        final_deck = []
+        half_deck = len(card_deck) // 2
+        left_half = card_deck[0:half_deck]
+        right_half = card_deck[half_deck::]
+
+        # Looping the 2 half's and append them to the list by values.
+        for card in range(len(left_half)):
+            final_deck.append(left_half[card])
+            final_deck.append(right_half[card])
+        card_deck = final_deck
+
+    print(card_deck)
+
+
+farro_shuffle(card_deck=input().split(), count_of_shuffles=int(input()))
